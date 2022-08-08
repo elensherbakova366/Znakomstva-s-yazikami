d8553501ac6514fb5518fa18558a7c1ecf829f58
ElenSherbakova366@yandex.ru <ElenSherbakova366Yandex.ru>
2022-08-08 19:22:17 +0400

    Пыталась решить задачу 2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine ("Введите числа 2, 44, 22");
+int numberA = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите числа 3, 5, 3");
+int numberB = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите числа 22, 3, 9");
+int number = int.Parse (Console.ReadLine());
+int max = numberA;
+Console.WriteLine("Сравним числа:");
+if (numberB < numberA > numberC)
+{   Console.WriteLine (numberA = max);
+    Console.WriteLine ("Число большее");
+}
+else if (numberB = numberA) 
+{
+     Console.WriteLine ("Числа равны");
+} 
+   
+else if (numberC = numberA) 
+{   
+    Console.WriteLine ("Числa равны");
+}
+else if (numberC = numberB) 
+{   
+    Console.WriteLine ("Числa равны");
+}
+else
+{
+    Console.WriteLine(numberB = max);
+    Console.WriteLine(numberC = max);
+     Console.WriteLine ("Число меньшее");
+}
