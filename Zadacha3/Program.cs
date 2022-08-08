50ce52378557e199d25baace3695d1afd097cedc
ElenSherbakova366@yandex.ru <ElenSherbakova366Yandex.ru>
2022-08-08 19:49:49 +0400

    Пыталась решить Задачу 3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите числа") +
+                        (4, -3, 7);
+int numberA = int.Parse(Console.ReadLine());
+if (numberA/2)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
+//тоже не получается. Пишет что "Возможно, аргумент-ссылка, допускающий значение NULL, для параметра "s" в "int int.Par
+//se(string s)"" 
