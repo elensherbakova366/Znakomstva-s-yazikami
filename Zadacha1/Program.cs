9e4d8b109ff658f9a0166f3e0de116305aac6168
ElenSherbakova366@yandex.ru <ElenSherbakova366Yandex.ru>
2022-08-10 13:33:59 +0400

    Задача1

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -1,9 +1,6 @@
-﻿Console.WriteLine ("Введите числа 5,2,-9");
-int numberA = int.Parse (Console.ReadLine());
-Console.WriteLine ("Введите числа 7,10,-3");
-int numberB = int.Parse (Console.ReadLine());
+﻿int numberA = -3;
+int numberB = 8;
 int max = numberA;
-Console.WriteLine("Сравним числа:");
 if (numberA > numberB)
 {   Console.WriteLine (numberA = max);
     Console.WriteLine ("Число большее");
@@ -13,6 +10,3 @@ else
     Console.WriteLine(numberB = max);
     Console.WriteLine ("Число меньшее");
 }
-// я хотела решить задачку таким образом, что переменные не надо вводить, 
-//а чтоб они уже были заданы, но как записать это, так и не сообразила. Может 
-//подскажите в коммнтарии? 
