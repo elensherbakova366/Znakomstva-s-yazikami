7c8a061d275297b05de4cfe0dbb9bc3e0f40d09d
ElenSherbakova366@yandex.ru <ElenSherbakova366Yandex.ru>
2022-08-21 22:50:06 +0400

    Добавила Задача1 после 3 семинара

---
+++ Zadacha1.3/Program.cs
@@ -0,0 +1,26 @@
+﻿// int[]array = new int [5];
+// Console.WriteLine("Введите пятизначное число");
+// int numb = int.Parse(Console.ReadLine());
+// for (int i = 0; i<array.Length; i++)
+// {
+//     if (array [0] == array[4] && array [1] == array [3])
+//     {
+//         Console.WriteLine("Число является палиндромом");
+//     }
+//     else
+//     {
+//       Console.WriteLine("Число не является палиндромом");  
+//     }
+// }
+// Какая-то у меня опять ерунда!Не понимаю,в чем у меня ошибка.
+// Напиши,пожалуйста, в комментарии
+string str = "123331";
+if(str [0] == str [5] && str [1] == str [4] && str[2] == str[3])   
+{                                             
+  Console.WriteLine("Это палиндром");       
+}
+else
+{
+  Console.WriteLine("Это не палиндром");
+}
+//А эту я после просмотра лекции написала, можно сказать, у тебя списала)
